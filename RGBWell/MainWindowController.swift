21919019dd223ff2330999c4546b016a20c42e9e
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-07 17:06:19 -0400

    Main window showing

---
+++ RGBWell/MainWindowController.swift
@@ -0,0 +1,15 @@
+//
+//  MainWindowController.swift
+//  RGBWell
+//
+//  Created by Stephen Skubik-Peplaski on 9/7/15.
+//  Copyright (c) 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    override var windowNibName: String? {
+        return "MainWindowController"
+    }
+}
