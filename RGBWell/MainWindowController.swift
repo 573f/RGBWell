05da1be52072ba03efe0aee884f9d0fafb8c4faa
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-07 20:16:24 -0400

    Got RGBA sliders working

--- RGBWell/MainWindowController.swift
+++ RGBWell/MainWindowController.swift
@@ -9,7 +9,43 @@
 import Cocoa
 
 class MainWindowController: NSWindowController {
+    
+    var r: Float = 0.0
+    var b: Float = 0.0
+    var g: Float = 0.0
+    var a: Float = 1.0
+    
+    @IBOutlet weak var colorWell: NSColorWell!
+    
     override var windowNibName: String? {
         return "MainWindowController"
+    }
+    
+    func updateColor() {
+        let newColor = NSColor(calibratedRed: CGFloat(r),
+                                       green: CGFloat(g),
+                                        blue: CGFloat(b),
+                                       alpha: CGFloat(a))
+        colorWell.color = newColor
+    }
+    
+    @IBAction func adjustRed(sender: NSSlider) {
+        r = sender.floatValue
+        updateColor()
+    }
+
+    @IBAction func adjustGreen(sender: NSSlider) {
+        g = sender.floatValue
+        updateColor()
+    }
+    
+    @IBAction func adjustBlue(sender: NSSlider) {
+        b = sender.floatValue
+        updateColor()
+    }
+    
+    @IBAction func adjustAlpha(sender: NSSlider) {
+        a = sender.floatValue
+        updateColor()
     }
 }
